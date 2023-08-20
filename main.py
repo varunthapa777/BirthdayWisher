7e072288fb3f8f4d6a19c363e209ba07c4d27aad
VarunThapa <140646101+varunthapa777@users.noreply.github.com>
2023-08-20 17:53:07 +0000

    mod main and automation

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import os
 import smtplib
 
 try:
-    SOME_SECRET = os.environ["MY_SECRET_TOKEN"]
+    SECRET_TOKEN = os.environ["MY_SECRET_TOKEN"]
+    EMAIL = os.environ["EMAIL"]
 except KeyError:
     SOME_SECRET = "Token not available!"
 
-MY_EMAIL = "varunthapa615@gmail.com"
-PASSWORD = SOME_SECRET
+
 connection = smtplib.SMTP("smtp.gmail.com")
 
 connection.starttls()
-connection.login(user=MY_EMAIL, password=PASSWORD)
+connection.login(user=EMAIL, password=SECRET_TOKEN)
 
-connection.sendmail(from_addr=MY_EMAIL, to_addrs="varunthapa1920@gmail.com", msg="Subject:Hemlo!\n\nThis is Varunthapa")
+connection.sendmail(from_addr=EMAIL, to_addrs="varunthapa1920@gmail.com", msg="Subject:Hemlo!\n\nThis is Varunthapa")
 connection.close()
