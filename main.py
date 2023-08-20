d56130495b544ff361f72f581a9d487bf671402d
VarunThapa <140646101+varunthapa777@users.noreply.github.com>
2023-08-20 17:42:25 +0000

    mod main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import os
+import smtplib
 
 try:
     SOME_SECRET = os.environ["MY_SECRET_TOKEN"]
 except KeyError:
     SOME_SECRET = "Token not available!"
-print("Hello")
-print(SOME_SECRET)
+
+MY_EMAIL = "varunthapa615@gmail.com"
+PASSWORD = SOME_SECRET
+connection = smtplib.SMTP("smtp.gmail.com")
+
+connection.starttls()
+connection.login(user=MY_EMAIL, password=PASSWORD)
+
+connection.sendmail(from_addr=MY_EMAIL, to_addrs="varunthapa1920@gmail.com", msg="Subject:Hemlo!\n\nThis is Varunthapa")
+connection.close()
